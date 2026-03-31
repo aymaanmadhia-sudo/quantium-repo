760a3f894eb1a6087c7ee4e9bffe7af41f77a7bf
aymaanmadhia-sudo <aymaan.madhia@gmail.com>
2026-03-31 18:56:50 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,22 @@
+import pandas as pd
+
+df1 = pd.read_csv("daily_sales_data_0.csv")
+df2 = pd.read_csv("daily_sales_data_1.csv")
+df3 = pd.read_csv("daily_sales_data_2.csv")
+
+
+df = pd.concat([df1, df2, df3])
+
+
+df = df[df["product"] == "pink morsel"]
+
+
+df["price"] = df["price"].replace('[\$,]', '', regex=True).astype(float)
+
+df["sales"] = df["price"] * df["quantity"]
+df = df[["sales", "date", "region"]]
+
+
+df.to_csv("combined.csv", index=False)
+
+print(df)
